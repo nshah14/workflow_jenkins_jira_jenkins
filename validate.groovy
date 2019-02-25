346320ff6b07b0f2bc893bce7c89557427bd9439
naved shah <naved.shah@uk.fujitsu.co.uk>
2019-02-25 12:00:09 +0000

    PS-8

--- validate.groovy
+++ validate.groovy
@@ -44,26 +44,27 @@ import groovy.json.JsonSlurper
 
     def createJson(name, value ){
         def jsonSlurper = new JsonSlurper()
-        def object = jsonSlurper.parseText('{
-                release: "poa-bal",
-                version: "18.2",
-                artifacts:
-                [
-                    {
-                        artifact_name: "abcd",
-                        artifact_version: 12.2
-                    },
-                    {
-                        artifact_name: "xyz",
-                        artifact_version: 12.2
-                    },
-                    {
-                        artifact_name: "efgh",
-                        artifact_version: 12.2
-                    }
-                ]
+        def object = jsonSlurper.parseText('{ 
 
-            }')
+    "release": "poa-bal",
+    "version": "18.2",
+    "artifacts":
+    [
+        {
+            "artifact_name": "abcd",
+            "artifact_version": 12.2
+        },
+        {
+            "artifact_name": "xyz",
+            "artifact_version": 12.2
+        },
+        {
+            "artifact_name": "efgh",
+            "artifact_version": 12.2
+        }
+    ]
+
+        } ')
         println('Object '+object)
     }
 
